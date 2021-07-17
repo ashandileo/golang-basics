aed2a072e143a0312b81b18b3cc201ec53cc03b4
Ashandi Leonadi <ashandileonadi2001@gmail.com>
2021-07-17 15:44:26 +0700

    arrays and slices

--- main.go
+++ main.go
@@ -3,31 +3,25 @@ package main
 import "fmt"
 
 func main() {
+	// Array
+	var ages [3]int = [3]int{20, 25, 30} // array with integer type and 3 length
+	var agesTwo = [3]int{20, 25, 30}     // same with above
+	fmt.Println(ages, agesTwo)
+	fmt.Println("array length is: ", len(ages)) // two print array length
 
-	age := 19
-	name := "Ashandi"
+	// shorthand to create an array
+	names := [4]string{"ashandi", "lutpi", "eman", "palah"}
+	names[1] = "adam"
+	fmt.Println(names, len(names))
 
-	// Print
-	// doesn't add a new line, if you want to add a new line use the (\n)
-	fmt.Print("Hello \n")
-	fmt.Print("World \n")
+	// Slices (use arrays under the hood)
+	// slice can append
+	var scores = []int{100, 50, 30}
+	scores[2] = 25
+	scores = append(scores, 85)
+	fmt.Println("Slice: ", scores)
 
-	// Println
-	// will add a new line automatically at the end
-	fmt.Println("Hello")
-	fmt.Println("new line")
-
-	fmt.Println("my age is", age, "and my name is", name)
-
-	// Printf (Formatted string) -> way to create a string with variables embedded inside it
-	// %_ = format specifier
-	fmt.Printf("my age is %v and my name is %v \n", age, name) // %v default
-	fmt.Printf("my age is %q and my name is %q \n", age, name) // %q will add double qoute for string var
-	fmt.Printf("age is of type %T \n", age)                    // get the types of variable
-	fmt.Printf("your scored %f points! \n", 225.25)            // float
-	fmt.Printf("your scored %0.2f points! \n", 225.25555)      // float with 2 decimal
-
-	// Sprintf (save formated string)
-	var str = fmt.Sprintf("my age is %v and my name is %v \n", age, name)
-	fmt.Println("the save string is: ", str)
+	// Slice ranges -> way to get a range of elements
+	rangeOne := names[1:]
+	fmt.Println(rangeOne)
 }
