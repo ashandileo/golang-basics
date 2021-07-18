6b1f5545de215287fcc550f3cc034ee7b78199da
Ashandi Leonadi <ashandileonadi2001@gmail.com>
2021-07-18 11:30:44 +0700

    pass by value

--- main.go
+++ main.go
@@ -2,39 +2,19 @@ package main
 
 import "fmt"
 
-var score = 99.5
+func updateMenu(menu map[string]float64) {
+	menu["coffe"] = 2.99
+}
 
 func main() {
 
-	// Map -> allows us to store key value pairs where the key and values can be different type
-	// But in a single map all of the keys and value must be the same type
-
-	menu := map[string]float64{ // type of key and type of value
-		"soup":          4.99,
-		"pie":           7.99,
-		"salad":         6.99,
-		"toffe pudding": 3.55,
+	// Group B => slices, maps, functions
+	// will change the original value
+	menu := map[string]float64{
+		"pie":       5.95,
+		"ice cream": 3.99,
 	}
 
-	fmt.Println(menu)
-	fmt.Println(menu["pie"])
-
-	// looping map
-	for key, value := range menu {
-		fmt.Println(key, "-", value)
-	}
-
-	// int as key type
-	phoneBook := map[int]string{
-		8811882850: "ashandi",
-		8828128910: "alfalah",
-		9219179312: "dani",
-	}
-
-	fmt.Println(phoneBook)
-	fmt.Println(phoneBook[8811882850])
-
-	// Update map value
-	phoneBook[8811882850] = "leonadi"
-	fmt.Println(phoneBook)
+	updateMenu(menu)
+	fmt.Println(menu) // we will see coffe as the new value
 }
